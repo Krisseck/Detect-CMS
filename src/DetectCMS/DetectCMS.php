0073386850899b07c80fd6cf4ae70b16ea7b4f7e
Stu Goodman <stuart@wfuk.com>
2018-06-18 11:22:05 +0100

    Added Laravel

--- src/DetectCMS/DetectCMS.php
+++ src/DetectCMS/DetectCMS.php
@@ -1,187 +1,188 @@
 <?php
+
 namespace DetectCMS;
 
-require_once __DIR__.'/Thirdparty/simple_html_dom.php';
+require_once __DIR__ . '/Thirdparty/simple_html_dom.php';
 
-class DetectCMS {
+class DetectCMS
+{
 
-	public $systems = array(
-    "Drupal",
-    "Wordpress",
-    "Joomla",
-    "Liferay",
-    "vBulletin",
-    "Magento",
-    "Magento2",
-    "ExpressionEngine",
-    "Sitecore",
-    'Bohemiasoft',
-    'EshopRychle',
-    'Fastcentrik',
-    'Shopsys',
-    'Shoptet',
-    'Webgarden',
-    'Webnode',
-    "Moodle",
-    "Modx"
-  );
+    public $systems = [
+        "Drupal",
+        "Wordpress",
+        "Joomla",
+        "Liferay",
+        "vBulletin",
+        "Magento",
+        "ExpressionEngine",
+        "Sitecore",
+        'Bohemiasoft',
+        'EshopRychle',
+        'Fastcentrik',
+        'Shopsys',
+        'Shoptet',
+        'Webgarden',
+        'Webnode',
+        'Laravel',
+    ];
 
-	private $common_methods = array("generator_header","generator_meta");
+    private $common_methods = ["generator_header", "generator_meta"];
 
-	public $home_html = "";
+    public $home_html = "";
 
-	public $home_headers = array();
+    public $home_headers = [];
 
-	public $url = "";
-	
-	public $result = FALSE;
+    public $url = "";
 
-	function __construct($url) {
-		$this->url = $url;
-		list($this->home_headers, $this->home_html) = $this->fetchBodyAndHeaders();
-		$this->result = $this->check($url);
-	}
+    public $result = false;
 
-	public function check() {
+    function __construct($url)
+    {
+        $this->url = $url;
+        list($this->home_headers, $this->home_html) = $this->fetchBodyAndHeaders();
+        $this->result = $this->check($url);
+    }
 
-		/*
-		 * Common, easy way first: check for Generator metatags or Generator headers
-		 */
-				
-		foreach($this->systems as $system_name) {
+    public function check()
+    {
 
-      $system_class = 'DetectCMS\\Systems\\'.$system_name;
+        /*
+         * Common, easy way first: check for Generator metatags or Generator headers
+         */
 
-			$system = new $system_class($this->home_html, $this->home_headers, $this->url);
+        foreach ($this->systems as $system_name) {
 
-			foreach($this->common_methods as $method) {
+            $system_class = 'DetectCMS\\Systems\\' . $system_name;
 
-				if(method_exists($system,$method)) {
+            $system = new $system_class($this->home_html, $this->home_headers, $this->url);
 
-					if($system->$method()) {
+            foreach ($this->common_methods as $method) {
 
-            return $system_name;
+                if (method_exists($system, $method)) {
 
-          }
+                    if ($system->$method()) {
 
-				}
+                        return $system_name;
 
-			}
+                    }
 
-		}
+                }
 
-		/*
-		 * Didn't find it yet, let's just use regular tricks
-		 */
+            }
 
-		foreach($this->systems as $system_name) {
+        }
 
-      $system_class = 'DetectCMS\\Systems\\'.$system_name;
+        /*
+         * Didn't find it yet, let's just use regular tricks
+         */
 
-			$system = new $system_class($this->home_html, $this->home_headers, $this->url);
+        foreach ($this->systems as $system_name) {
 
-			foreach($system->methods as $method) {
+            $system_class = 'DetectCMS\\Systems\\' . $system_name;
 
-				if(!in_array($method,$this->common_methods)) {
+            $system = new $system_class($this->home_html, $this->home_headers, $this->url);
 
-					if($system->$method()) {
-						return $system_name;
+            foreach ($system->methods as $method) {
 
-					}
+                if (!in_array($method, $this->common_methods)) {
 
-				}
+                    if ($system->$method()) {
+                        return $system_name;
 
-			}
+                    }
 
-		}
+                }
 
-		return FALSE;
+            }
 
-	}
+        }
 
-	public function getResult() {
+        return false;
 
-		return $this->result;
+    }
 
-	}
+    public function getResult()
+    {
+        return $this->result;
+    }
 
-	protected function fetch($url=null) {
+    protected function fetch($url = null)
+    {
 
-		$ch = curl_init();
+        $ch = curl_init();
 
-		if($url==null) {
-			$url = $this->url;
-		}
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        if ($url == null) {
+            $url = $this->url;
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
 
-		$return = curl_exec($ch);
+        $return = curl_exec($ch);
 
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		if($httpCode == 404) {
-			curl_close($ch);
-			return FALSE;
-		}
+        if ($httpCode == 404) {
+            curl_close($ch);
+            return false;
+        }
 
-		curl_close($ch);
-	
-		return $return;
+        curl_close($ch);
 
-	}
+        return $return;
 
-	protected function fetchBodyAndHeaders() {
+    }
 
-		$ch = curl_init();
+    protected function fetchBodyAndHeaders()
+    {
 
-		curl_setopt($ch, CURLOPT_URL, $this->url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_HEADER, 1);
+        $ch = curl_init();
 
-		$response = curl_exec($ch);
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
 
-		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $response = curl_exec($ch);
 
-		if($httpCode == 404) {
-	    curl_close($ch);
-	    return FALSE;
-		}
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-		$header = substr($response, 0, $header_size);
-		$body = substr($response, $header_size);
+        if ($httpCode == 404) {
+            curl_close($ch);
+            return false;
+        }
 
-		$header_array = array();
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
 
-		foreach (explode("\r\n", $header) as $line) {
-			if($line == '')
-      {
-          continue;
-      }
+        $header_array = [];
 
-      $array = explode(': ', $line);
-      if(array_key_exists(1,$array))
-      {
-        list ($key, $value) = $array;
-        $header_array[$key] = $value;
-        continue;
-      }
+        foreach (explode("\r\n", $header) as $line) {
+            if ($line == '') {
+                continue;
+            }
 
-			$header_array['http_code'] = $line;
-		}
+            $array = explode(': ', $line);
+            if (array_key_exists(1, $array)) {
+                list ($key, $value) = $array;
+                $header_array[$key] = $value;
+                continue;
+            }
 
-		curl_close($ch);
+            $header_array['http_code'] = $line;
+        }
 
-		return array($header_array, $body);		
+        curl_close($ch);
 
-	}
+        return [$header_array, $body];
+
+    }
 
 }
